3751976c14f7cbfa47097e5fef3d5397ddb2f1c4
Mikkel Flindt Heisterberg <lekkim@heisterberg.dk>
2022-10-12 11:54:27 +0200

    Implementing PKCE flow from browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,29 @@
+const navigationHandler_Login = async () => {
+    // ask server for login details
+    const resp = await fetch("/api/logindetails", {
+        method: "get",
+        headers: {
+            "content-type": "application/json",
+            accept: "application/json"
+        }
+    })
+    const obj = await resp.json();
+
+    // save in localStorage
+    localStorage.setItem("logindetails", JSON.stringify(obj));
+
+    // create state
+    const strstate = btoa(JSON.stringify({"foo": "bar"}));
+    
+    // initiate login
+    document.location.href = `https://${obj.mydomain}/services/oauth2/authorize?client_id=${obj.client_id}&redirect_uri=${obj.redirect_uri}&response_type=code&state=${strstate}&code_challenge=${obj.code_challenge}`;
+    
+}
+const navigationHandler_Logout = () => {
+    localStorage.removeItem("user");
+    document.location.hash = "#";
+};
+
 const addTagHeadline = (container, text) => {
     const elemHeadline = document.createElement("h1");
     elemHeadline.className = "cover-heading";
@@ -26,11 +52,26 @@ const addMenuItem = (container, title, hash) => {
         elem.appendChild(document.createTextNode(title));
         container.appendChild(elem);
 }
+const getUser = () => {
+    const struser = localStorage.getItem("user");
+    if (!struser) return undefined;
+    return JSON.parse(struser);
+}
 const buildMenu = () => {
+    // get user
+    const user = getUser();
+    console.log(`User`, user);
+
+    // build menu
     const navigationContainer = document.getElementById("navigation");
     navigationContainer.innerText = "";
     addMenuItem(navigationContainer, "Home", "");
     addMenuItem(navigationContainer, "About", "about");
+    if (!user) {
+        addMenuItem(navigationContainer, "Login", "login");
+    } else {
+        addMenuItem(navigationContainer, "Logout", "logout");
+    }
 }
 const navigationClickHandler = (ev) => {
     const hash = document.location.hash;
@@ -50,7 +91,10 @@ const navigationClickHandler = (ev) => {
             mainContainer,
             "Cras vitae elit in urna efficitur vulputate sed in felis. Ut imperdiet nisi id quam efficitur, non ultrices ante mattis. Aenean aliquet a velit in efficitur. Nunc elementum sit amet elit in lobortis. Donec varius mauris gravida neque maximus, at elementum orci euismod. Aliquam vestibulum nunc lacus, eget ultrices augue vulputate non. Nunc tincidunt cursus posuere. Aenean elementum lorem magna, sit amet tempus felis porttitor id. Maecenas ligula odio, ullamcorper eu pretium non, vehicula at lorem. Etiam sem lectus, scelerisque et fermentum at, hendrerit in tortor. Etiam id sem quis ipsum interdum ultrices vel in ipsum. In facilisis venenatis feugiat. Duis tincidunt turpis eu massa rhoncus dictum."
         );
-        
+    } else if (["#login"].includes(hash)) {
+        navigationHandler_Login();
+    } else if (["#logout"].includes(hash)) {
+        navigationHandler_Logout();
     }
 
 }
@@ -60,8 +104,48 @@ const initNavigation = () => {
     buildMenu();
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
     initNavigation();
     navigationClickHandler();
+
+    // look for logindetails info in local storage
+    const strlogindetails = localStorage.getItem("logindetails")
+    if (strlogindetails && (location.search.includes("state=") && (location.search.includes("code=") || location.search.includes("error=")))) {
+        const params = new URLSearchParams(location.search);
+        window.history.replaceState({}, document.title, "/");
+        const auth_code = params.get("code");
+        const strstate = params.get("state");
+        console.log("State", JSON.parse(atob(strstate)));
+
+        // get logindetails
+        const logindetails = JSON.parse(strlogindetails);
+
+        // exchange authcode
+        const respToken = await fetch(`https://${logindetails.mydomain}/services/oauth2/token`, {
+            "method": "post",
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+                "accept": "application/json"
+            },
+            body: `code=${auth_code}&code_verifier=${logindetails.code_verifier}&grant_type=authorization_code&redirect_uri=${logindetails.redirect_uri}&client_id=${logindetails.client_id}`
+        })
+        const tokeninfo = await respToken.json();
+        console.log(tokeninfo);
+
+        // get userinfo
+        const respUserinfo = await fetch(`https://${logindetails.mydomain}/services/oauth2/userinfo`, {
+                headers: {
+                    authorization: `Bearer ${tokeninfo.access_token}`,
+                },
+            }
+        );
+        const userinfo = await respUserinfo.json();
+
+        localStorage.setItem("user", JSON.stringify({
+            tokeninfo,
+            userinfo
+        }));
+        location.hash = "#";
+    }
 });
 window.addEventListener("hashchange", navigationClickHandler);
--- src/configure-routes-anonymous.ts
+++ src/configure-routes-anonymous.ts
@@ -1,18 +1,35 @@
-import { Application } from "express";
+import express, { Application } from "express";
+import { v4 as uuid } from "uuid";
+import {createHash, pseudoRandomBytes} from "crypto";
+import base64url from "base64url";
 
-const buildRenderContext = (headline: string, data? : any) : any => {
+const buildRenderContext = (data? : any) : any => {
     const std = {
         app: {
             title: process.env.APP_TITLE || "Salesforce Identity Hackathon"
-        },
-        headline
+        }
     }
     if (!data) return std;
     return Object.assign(std, data);
 }
 
+const router = express.Router();
+router.get("/logindetails", (_req, res) => {
+    const code_verifier = base64url(pseudoRandomBytes(32));
+    const code_challenge = base64url(createHash("sha256").update(code_verifier).digest());
+    res.type("json");
+    res.send({
+        "client_id": process.env.CLIENT_ID,
+        "redirect_uri": process.env.REDIRECT_URI,
+        "mydomain": process.env.MYDOMAIN,
+        code_challenge,
+        code_verifier
+    })
+});
+
 export default (app : Application) => {
     app.get("/", (req, res) => {
-        return res.render("root", buildRenderContext("Welcome!"));
+        return res.render("root", buildRenderContext());
     })
+    app.use("/api", router);
 }
